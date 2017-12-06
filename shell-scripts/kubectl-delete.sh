d8fd09e7c76c185d938666dda7fd9eb7107e509f
Christopher Zhu <christopher.zhu.9@gmail.com>
2017-12-06 12:09:02 -0500

    Finalize kubectl-delete.sh

--- shell-scripts/kubectl-delete.sh
+++ shell-scripts/kubectl-delete.sh
@@ -1,7 +1,20 @@
 #! /bin/bash
 
-kubectl delete --all deploy
-kubectl delete --all services
-kubectl delete --all jobs
-kubectl delete --all daemonsets
-kubectl delete --all po
+if [[ ( ${#} -ne 1 ) || ( -z ${1} ) ]]; then
+    NAMESPACE=$(kubectl config view --minify | grep 'namespace' | sed 's/^ *namespace: \([a-zA-Z0-9_-]*\)$/\1/' | tr -d '\n')
+else
+    NAMESPACE="${1}"
+fi
+
+echo "About to delete all deployments, services, jobs, daemonsets, and pods from ${NAMESPACE}."
+read -p "Are you sure? [y/N] " -r
+if [[ "${REPLY}" =~ [yY]$ ]]; then
+    # Using kubectl resource type shorthands
+    # - deployments -> deploy
+    # - services -> svc
+    # - daemonsets -> ds
+    # - po -> pods
+    kubectl delete --all --namespace="${NAMESPACE}" deploy,svc,jobs,ds,po
+else
+    echo "Aborted."
+fi
