3760bec5e9444e9da6b8e0aaa9c2b0c3fab0e28f
Christopher Zhu <christopher.zhu.9@gmail.com>
2018-07-04 14:31:55 -0400

    Add command line arguments for port to php script

--- shell-scripts/php-local-server.sh
+++ shell-scripts/php-local-server.sh
@@ -1,5 +1,25 @@
 #! /bin/bash
 
-#TODO: Add command line argument(s) for IP and Port
+IP_ADDR="0.0.0.0"
+PORT=8000
 
-php -S 0.0.0.0:8000
+if [ ${#} -eq 1 ]; then
+    # test if input is a number
+    if [[ "$1" =~ ^[0-9]+$ ]]; then
+        if [ \( ${1} -eq 0 \) -o \( \( ${1} -ge 1024 \) -a \( ${1} -le 65535 \) \) ]; then
+            # valid port value
+            echo "Setting php server address to $IP_ADDR:$1"
+            PORT=$1
+        else
+            echo "Port value was invalid."
+            exit 2
+        fi
+    else
+        echo "Input was not a number."
+        exit 1
+    fi
+else
+    echo "No port provided, defaulting to $IP_ADDR:$PORT"
+fi
+
+php -S "$IP_ADDR:$PORT"
