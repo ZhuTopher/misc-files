5e0fed8e438c5924db81588c1ca1fee5c806ea9a
Christopher Zhu <christopher.zhu.9@gmail.com>
2021-01-05 20:56:26 -0500

    Tweak xinput-mouse-pref.sh for new mouse

--- shell-scripts/xinput-mouse-pref.sh
+++ shell-scripts/xinput-mouse-pref.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
-# eSentire Lenovo mouse: Logitech Lenovo USB Optical Mouse
 # My personal mouse: Logitech USB Laser Mouse
+# My personal mouse: Logitech MX Anywhere 2S
 
-#MOUSE=$(xinput --list | grep -o "Logitech Lenovo USB Optical Mouse")
+#MOUSE=$(xinput --list | grep -o "Logitech MX Anywhere 2S")
 
-xinput set-prop "Logitech USB Laser Mouse" "Device Accel Profile" 0
-xinput set-prop "Logitech USB Laser Mouse" "Coordinate Transformation Matrix" 0.800000, 0.000000, 0.000000, 0.000000, 0.800000, 0.000000, 0.000000, 0.000000, 1.000000
+# xinput set-prop "Logitech USB Laser Mouse" "Device Accel Profile" 0
+
+# Adjust the coordinate transformation matrix: https://askubuntu.com/a/569345
+xinput set-prop "pointer:Logitech MX Anywhere 2S" "Coordinate Transformation Matrix" 0.800000, 0.000000, 0.000000, 0.000000, 0.800000, 0.000000, 0.000000, 0.000000, 1.000000
